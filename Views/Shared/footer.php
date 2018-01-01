464210bccf1d303fb124e2c2c3be874a80f087cb
ugork1 <karol.ugorcak@students.bfh.ch>
2018-01-01 18:21:20 +0100

    made eMail clickable

--- Views/Shared/footer.php
+++ Views/Shared/footer.php
@@ -4,12 +4,19 @@
                 <ul class="list-unstyled">
                     <li>Bern, Laupenstrasse 77</li>
                     <li> CH-3004</li>
-                    <li>info@computerstore.ch<li>
-                    <li>0794443322<li>
+                    <li>
+<!--                        --><?php
+//                            $msg = "First line of text";
+//                            $msg = wordwrap($msg,70);
+//                            $headers = "FROM: ugorcak@gmx.ch";
+//                            mail("info@computerstore.ch","My subject",$msg,$headers);
+//                        ?>
+                        <a style="color: white" href="mailto:info@computerstore.ch">info@computerstore.ch</a>
+                    </li>
+                    <li>0794443322</li>
                 </ul>
             </div>
             <div class="footer-block"></div>
         </footer>
-
     </body>
 </html>
